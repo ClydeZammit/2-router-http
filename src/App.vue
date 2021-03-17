6d1ed8c61106977439e0b7061f82620bc825eff5
czammit001 <clyde.zammit.c10334@mcast.edu.mt>
2021-03-17 13:56:49 +0100

    Nested Router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link class="home" :to="{name: 'TasksList'}">Tasks</router-link> |
+    <router-link :to="{name: 'About'}">About</router-link>
   </div>
   <router-view/>
 </template>
@@ -13,6 +13,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.home{
+  border: green solid 1px;
 }
 
 #nav {
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import TasksList from '../views/TasksList.vue'
+import TaskDetails from '../views/task/Details.vue'
+import TaskLayout from '../views/task/Layout.vue'
+import TaskEdit from '../views/task/Edit.vue'
+import TaskDelete from '../views/task/Delete.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'TasksList',
+    component: TasksList,
+    // if page exists parse the string to an integer otherwise return 1
+    props: route => ({ page: parseInt(route.query.page) || 1 })
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/tasks/:id',
+    name: 'TaskLayout',
+    props: true,
+    component: TaskLayout,
+    children: [
+      {
+        path: '',
+        name: 'TaskDetails',
+        component: TaskDetails
+      },
+      {
+        path: '/tasks/:id/edit',
+        name: 'TaskEdit',
+        props: true,
+        component: TaskEdit
+      },
+      {
+        path: '/tasks/:id/delete',
+        name: 'TaskDelete',
+        props: true,
+        component: TaskDelete
+      }
+    ]
   }
 ]
 
